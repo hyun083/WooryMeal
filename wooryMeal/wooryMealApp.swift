6124569350703b1d37b1860ef573cd305e4eb7a3
hyun <jh9501@gmail.com>
2024-12-16 21:41:57 +0900

    Initial Commit

---
+++ wooryMeal/wooryMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  wooryMealApp.swift
+//  wooryMeal
+//
+//  Created by Hyun on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct wooryMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
